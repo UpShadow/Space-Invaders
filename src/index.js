ad59a81b49590ef24fbdf5f674f21af57f2f6a8b
Carlos Eduardo Correia de Souza <CarlosCorreia1002@gmail.com>
2024-12-04 17:22:28 -0300

    Update 2
    Added spaceship images and sprites.
    Projectile class created.
    Ship rotation movement.
    Ship shooting implemented.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Player from "./classes/Player.js";
+import Projectile from "./classes/Projectile.js";
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -6,25 +7,73 @@ const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+ctx.imageSmoothingEnabled = false;
+
 const player = new Player(canvas.width, canvas.height);
+const playerProjectiles = [];
 
 const keys = {
     left: false,
     right: false,
+    shoot: {
+        pressed: false,
+        released: true,
+    },
+}
+
+const drawProjectiles = () => {
+    playerProjectiles.forEach((projectile) => {
+        projectile.draw(ctx);
+        projectile.update();
+    })
+}
+
+const clearProjectiles = () => {
+    playerProjectiles.forEach((projectile, index) => {
+        if(projectile.position.y <= 0) {
+            playerProjectiles.splice(index, 1)
+        }
+    });
 }
 
 const gameLoop = () => { 
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
+    drawProjectiles();
+    clearProjectiles();
+
+    ctx.save();
+
+    ctx.translate(
+        player.position.x + player.width / 2, 
+        player.position.y + player.height / 2
+    )
+
+    if(keys.shoot.pressed && keys.shoot.released) {
+        player.shoot(playerProjectiles);
+        keys.shoot.released = false;
+        console.log(playerProjectiles);
+    }
+
     if (keys.left && player.position.x >= 0) {
         player.moveLeft();
+        ctx.rotate(-0.15)
     }
 
-    if (keys.right && player.position.x <= canvas.width - 100) {
+    if (keys.right && player.position.x <= canvas.width - player.width) {
         player.moveRight();
+        ctx.rotate(+0.15)
     }
+
+    ctx.translate(
+        - player.position.x - player.width / 2, 
+        - player.position.y - player.height / 2
+    )
 
     player.draw(ctx);
+
+    ctx.restore()
+    
 
     requestAnimationFrame(gameLoop);
 }
@@ -34,6 +83,7 @@ addEventListener("keydown", () => {
     
     if(key === "a") keys.left = true;
     if(key === "d") keys.right = true;
+    if(key === "enter") keys.shoot.pressed = true;
 })
 
 addEventListener("keyup", () => {
@@ -41,6 +91,10 @@ addEventListener("keyup", () => {
     
     if(key === "a") keys.left = false;
     if(key === "d") keys.right = false;
-})
+    if(key === "enter") {
+        keys.shoot.pressed = false;
+        keys.shoot.released = true;
+    }
+});
 
 gameLoop();
